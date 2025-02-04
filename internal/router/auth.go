9a803b1b0ff9433724ad712e75552df1e68d7437
Sanito Gonzalez <ddalkisoup23@gmail.com>
2025-02-04 22:32:31 +0900

    [feat] Add account/bot and register/bot

--- internal/router/auth.go
+++ internal/router/auth.go
@@ -3,6 +3,7 @@ package router
 import (
 	"database/sql"
 	"errors"
+	"log"
 	"net/http"
 	"spire/lobby/internal/core"
 
@@ -19,19 +20,14 @@ type AuthResponse struct {
 }
 
 func HandleAuthBot(c *gin.Context, ctx *core.Context) {
-	var request AuthRequest
-	if err := c.Bind(&request); err != nil {
+	var r AuthRequest
+	if err := c.Bind(&r); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{})
 		return
 	}
 
-	queryChan := make(chan error, 1)
-	go func() {
-		row := ctx.Db.QueryRow("SELECT 1 FROM accounts WHERE account_id = ?", request.AccountId)
-		queryChan <- row.Scan()
-		close(queryChan)
-	}()
-	if err := <-queryChan; err != nil {
+	row := ctx.Db.QueryRow("SELECT 1 FROM accounts WHERE account_id = ?", r.AccountId)
+	if err := row.Scan(); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid account ID"})
 			return
@@ -41,7 +37,7 @@ func HandleAuthBot(c *gin.Context, ctx *core.Context) {
 	}
 
 	t := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"account_id": request.AccountId,
+		"account_id": r.AccountId,
 	})
 	s, err := t.SignedString(ctx.Settings.AuthKey)
 	if err != nil {
@@ -51,5 +47,96 @@ func HandleAuthBot(c *gin.Context, ctx *core.Context) {
 
 	c.JSON(http.StatusOK, AuthResponse{
 		Token: s,
+	})
+}
+
+func HandleAccountBot(c *gin.Context, ctx *core.Context) {
+	type Request struct {
+		BotId uint64 `json:"bot_id" binding:"required"`
+	}
+
+	type Response struct {
+		AccountId uint64 `json:"account_id"`
+	}
+
+	var r Request
+	if err := c.Bind(&r); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+
+	row := ctx.Db.QueryRow("SELECT account_id FROM bots WHERE bot_id = ?", r.BotId)
+	var accountId uint64
+	if err := row.Scan(&accountId); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			c.JSON(http.StatusBadRequest, Response{AccountId: 0})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, Response{AccountId: accountId})
+}
+
+func HandleRegisterBot(c *gin.Context, ctx *core.Context) {
+	type Request struct {
+		BotId uint64 `json:"bot_id" binding:"required"`
+	}
+
+	type Response struct {
+		AccountId uint64 `json:"account_id"`
+	}
+
+	var r Request
+	if err := c.Bind(&r); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+
+	// Register bot
+	tx, err := ctx.Db.Begin()
+	cleanup := func() {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
+		err := tx.Rollback()
+		if err != nil {
+			log.Fatalf("Error rolling back transaction: %v", err)
+		}
+	}
+	if err != nil {
+		cleanup()
+		return
+	}
+
+	row := tx.QueryRow("SELECT 1 FROM bots WHERE bot_id = ?", r.BotId)
+	if err := row.Scan(); !errors.Is(err, sql.ErrNoRows) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bot already registered"})
+		return
+	}
+
+	res, err := tx.Exec("INSERT INTO accounts (account_id) VALUES (?)", r.BotId)
+	if err != nil {
+		cleanup()
+		return
+	}
+	accountId, err := res.LastInsertId()
+	if err != nil {
+		cleanup()
+		return
+	}
+
+	_, err = tx.Exec("INSERT INTO bots (bot_id, account_id) VALUES (?, ?)", r.BotId, accountId)
+	if err != nil {
+		cleanup()
+		return
+	}
+
+	if err := tx.Commit(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Transaction error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, Response{
+		AccountId: uint64(accountId),
 	})
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -9,6 +9,8 @@ func NewRouter(ctx *core.Context) *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/auth/bot", func(c *gin.Context) { HandleAuthBot(c, ctx) })
+	r.GET("/account/bot", func(c *gin.Context) { HandleAccountBot(c, ctx) })
+	r.GET("/register/bot", func(c *gin.Context) { HandleRegisterBot(c, ctx) })
 
 	return r
 }
